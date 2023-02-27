41e214d3ae66b71103cd896a1c674fb5024b2e5d
Rafael Matos <124383453+rafarito@users.noreply.github.com>
2023-02-27 15:52:26 -0300

    Update main.java

--- main.java
+++ main.java
@@ -4,27 +4,33 @@ import javax.swing.JOptionPane;
 public class Aulaclass {
 
     public static void main(String[] args) {
-        Operacao operacao = new Operacao(); //faz as operações e informa ao usuario
+        Operacao operacao = new Operacao();// retorna o valor da operação
         double a,b;
-        String o;
-        a = Double.parseDouble(JOptionPane.showInputDialog(null,"digite o primeiro numero", "numero 1",3));
-        b = Double.parseDouble(JOptionPane.showInputDialog(null,"digite o segundo numero", "numero 2",3));
+        String o,temp;
+        do{
+          temp = JOptionPane.showInputDialog(null,"digite o primeiro numero", "numero 1",3);  
+        }while(temp.matches("[a-zA-Z]+"));
+        a = Double.parseDouble(temp);
+        do{
+          temp = JOptionPane.showInputDialog(null,"digite o segundo numero", "numero 2",3);
+        }while(temp.matches("[a-zA-Z]+"));
+        b = Double.parseDouble(temp);
         do{
              o = JOptionPane.showInputDialog(null,"digite a operação matematica\n\n+ - adição\n- - subtração\n* - multiplicação\n/ - divsão", "operação",3);
         }while(!((o.equals("+"))||(o.equals("-"))||(o.equals("*"))||(o.equals("/"))));
         
         switch(o){
             case "+":
-                operacao.adicao(a, b);
+                JOptionPane.showMessageDialog(null, "o resultado é "+ operacao.adicao(a, b) +"", "resultado", 1);
             break;
             case "-":
-                operacao.subtracao(a, b);
+                JOptionPane.showMessageDialog(null, "o resultado é "+ operacao.subtracao(a, b) +"", "resultado", 1);
             break;
             case "/":
-                operacao.divisao(a, b);
+                JOptionPane.showMessageDialog(null, "o resultado é "+ operacao.divisao(a, b) +"", "resultado", 1);
             break;
             case "*":
-                operacao.multiplicacao(a, b);
+                JOptionPane.showMessageDialog(null, "o resultado é "+ operacao.multiplicacao(a, b) +"", "resultado", 1);
             break;
                     
         }
